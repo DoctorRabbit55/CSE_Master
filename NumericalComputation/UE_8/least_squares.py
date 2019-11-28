5bae57d635d5399a244fa68a46ee45e93b386d26
Bernhard Haas <bernhardhaas55@gmail.com>
2019-11-28 13:11:22 +0100

    <Numerical Computation> 8.UE
    Fixed 8.3.

--- NumericalComputation/UE_8/least_squares.py
+++ NumericalComputation/UE_8/least_squares.py
@@ -2,15 +2,17 @@ import numpy as np
 import numpy.linalg as linalg
 import random
 
-N = np.arange(2,11)
+n = np.arange(2, 11)
 
-for i in range(len(N)):
+for i in n:
 
-    A = np.zeros((N[i],2))
-    y = np.zeros((N[i],1))
+    N = 2**i
+    
+    A = np.zeros((i,2))
+    y = np.zeros((i,1))
 
-    for idx in range(N[i]):
-        x = random.random()*1/(float)(len(N)) - 1/(float)(len(N))
+    for idx in range(i):
+        x = random.random() * 2/(float)(N) - 1/(float)(N)
         
         A[idx, 0] = x
         A[idx, 1] = x**3
@@ -23,5 +25,3 @@ for i in range(len(N)):
     sol = linalg.solve(X, b)
        
     print(sol)
-
-
